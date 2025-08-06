2e3abc30cd4d526e160290e5dddd154469e507c1
unknown <user@DESKTOP-FJJ4VUN.(none)>
2025-08-06 22:48:56 +0300

    edit Layout

--- views/layouts/Layout.jsx
+++ views/layouts/Layout.jsx
@@ -1,20 +1,25 @@
-const React = require('react')
+const React = require('react');
 
-function Layout(props){
- return(
-    <html>
-        <head>
-            <title>{!props.fruit?.name ? 'Fruits App - The Greatest Of All Time' : `${props.fruit.name} - Fruits App`}</title>
-            <link rel="stylesheet" href="/styles.css" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        </head>
-        <body>
-            <div className="container">
-                {props.children}
-            </div>
-        </body>
+function Layout(props) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Curriculum Guide for Degree Program</title>
+        <link rel="stylesheet" href="/style.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <div className="container">
+          <header>
+            <h1>🎓 Curriculum Guide</h1>
+          </header>
+          <main>
+            {props.children}
+          </main>
+        </div>
+      </body>
     </html>
- )
+  );
 }
 
-module.exports = Layout
+module.exports = Layout;
